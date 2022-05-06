9304d30697981a004bd412a4ebc53f61097f652d
Beo <32326640+Beowolve@users.noreply.github.com>
2022-05-06 08:29:18 +0200

    Update index.js
    Removed chrom-aws-lambda dependency and changed from puppeteer-core to puppeteer

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,39 @@
 const functions = require("firebase-functions");
-
-const chromium = require("chrome-aws-lambda");
-const puppeteer = chromium.puppeteer;
+const puppeteer = require('puppeteer');
 
 // admin seems to be necessary in order
 // to run the function via `firebase emulators`
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+// should increase performance a little
+const minimal_args = [
+  '--Disable-dev-shm-usage',//Create temporary file shared memory
+  '--Disable-setuid-sandbox',//uid sandbox
+  '–-No-first-run',//No home page is set. At startup, a blank page will open.
+  '--No-sandbox',//sandbox mode
+  '–-No-zygote',
+];
+
 exports.preview = functions
   .runWith({ memory: "512MB", timeoutSeconds: 10 })
   .https.onRequest(async (req, res) => {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: { width: 400, height: 300 },
-      executablePath: await chromium.executablePath,
-      headless: true,
-    });
+	const browser = await puppeteer.launch({
+	  defaultViewport: { width: 400, height: 300 },
+	  args: minimal_args,
+	  headless: true
+	});
 
-    const {
-      query: { q = "" },
-    } = req;
+	const {
+	  query: { q = "" },
+	} = req;
+	
+	const page = await browser.newPage();
 
-    const page = await browser.newPage();
+	await page.setContent(q);
+	const screenshot = await page.screenshot();
+	await browser.close();
 
-    await page.setContent(q);
-    const screenshot = await page.screenshot();
-    await browser.close();
-
-    res.header({ "Content-Type": "image/png" });
-    res.end(screenshot, "binary");
+	res.header({ "Content-Type": "image/png" });
+	res.end(screenshot, "binary");
   });
